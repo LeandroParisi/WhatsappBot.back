fe39edfb31689c459ba6d81fa97d391b2a251dbb
LeandroParisi <lepcbelisario@gmail.com>
2021-06-20 15:45:42 -0300

    setup: fix properlly sets coupons and promotions-products table

---
+++ migrations/20210620184446-creates-promotions-products-table.js
@@ -0,0 +1,45 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const transaction = await queryInterface.sequelize.transaction();
+
+    try {
+      await queryInterface.createTable('Promotions_Products', {
+        promotionId: {
+          allowNull: false,
+          foreignKey: true,
+          type: Sequelize.INTEGER,
+          onUpdate: 'CASCADE',
+          onDelete: 'CASCADE',
+          references: {
+            model: 'Promotions',
+            key: 'id',
+          },
+        },
+        productId: {
+          allowNull: false,
+          foreignKey: true,
+          type: Sequelize.INTEGER,
+          onUpdate: 'CASCADE',
+          onDelete: 'CASCADE',
+          references: {
+            model: 'Products',
+            key: 'id',
+          },
+        },
+      }, { transaction });
+
+
+      await transaction.commit();
+
+    } catch(error) {
+      await transaction.rollback();
+      throw error
+    }
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Promotions_Products');
+  }
+};
