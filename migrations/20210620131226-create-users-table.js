246c663b0dc7e263e99d1d0daf9d5cf84d68d8a6
LeandroParisi <lepcbelisario@gmail.com>
2021-06-20 11:04:16 -0300

    setup: creates Users migration

---
+++ migrations/20210620131226-create-users-table.js
@@ -0,0 +1,92 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const transaction = await queryInterface.sequelize.transaction();
+
+    try {
+      await queryInterface.createTable('Users', {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
+        },
+        whatsappNumber: {
+          allowNull: false,
+          type: Sequelize.STRING,  
+          unique: true,
+        },
+        whatsappId: {
+          allowNull: false,
+          type: Sequelize.STRING,  
+          unique: true,
+        },
+        bussinessName: {
+          allowNull: false,
+          type: Sequelize.STRING,
+        },
+        cnpj: {
+          allowNull: false,
+          type: Sequelize.STRING,
+        },
+        ownerFirstName: {
+          allowNull: false,
+          type: Sequelize.STRING,
+        },
+        ownerMiddleName: {
+          type: Sequelize.STRING,
+        },
+        ownerLastName: {
+          allowNull: false,
+          type: Sequelize.STRING,
+        },
+        ownerCpf: {
+          allowNull: false,
+          type: Sequelize.STRING,
+        },
+        passwordHash: {
+          allowNull: false,
+          type: Sequelize.STRING,
+        },
+        botName: {
+          allowNull: false,
+          type: Sequelize.STRING,
+          defaultValue: 'Walle',
+        },
+        email: {
+          allowNull: false,
+          unique: true,
+          type: Sequelize.STRING,
+        },
+        logo: {
+          type: Sequelize.STRING,
+        },
+        updatedAt: {
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.fn('now'),
+        },
+        createdAt: {
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.fn('now'),
+        },
+      }, { transaction });
+
+      await queryInterface.addIndex('Users', ['whatsappNumber'], { transaction });
+
+      await queryInterface.addIndex('Users', ['whatsappId'], { transaction });
+
+      await queryInterface.addIndex('Users', ['cnpj'], { transaction });
+
+      await transaction.commit();
+
+    } catch(error) {
+      await transaction.roolback();
+      throw error
+    }
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Users');
+  }
+};
