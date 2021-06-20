7c7e67f15eecf7bbe58ec3180b41fc9e13ed3334
LeandroParisi <lepcbelisario@gmail.com>
2021-06-20 16:00:20 -0300

    setup: fix, corrects promotions table type

--- migrations/20210620181803-creates-promotions-table.js
+++ migrations/20210620181803-creates-promotions-table.js
@@ -25,7 +25,7 @@ module.exports = {
           type: Sequelize.STRING,
         },
         dueDate: {
-          type: Sequelize.DATA,
+          type: Sequelize.DATE,
         },
         avaiability: {
           allowNull: true,
