06c5858fb5b9b5f449f5f1dea74618494abfbe87
LeandroParisi <lepcbelisario@gmail.com>
2021-11-08 09:00:00 -0300

    setup: updates config sequelize for dev environment

--- config/config.js
+++ config/config.js
@@ -12,14 +12,28 @@ module.exports = {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DATABASE_NAME,
-    host: process.env.DATABASE_URL,
+    host: process.env.HOSTNAME,
     dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
   production: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DATABASE_NAME,
-    host: process.env.DATABASE_URL,
+    host: process.env.HOSTNAME,
     dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
 };
