6beb8b3ed71229b020183a05884f179975d24e2f
LeandroParisi <lepcbelisario@gmail.com>
2022-02-06 13:44:43 -0300

    REFACTOR: config file changes

--- config/config.js
+++ config/config.js
@@ -5,14 +5,14 @@ module.exports = {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: 'whatsapp_bot_development',
-    host: process.env.HOSTNAME,
+    host: process.env.DB_HOST,
     dialect: 'postgres',
   },
   development: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DATABASE_NAME,
-    host: process.env.HOSTNAME,
+    host: process.env.DB_HOST,
     dialect: 'postgres',
     protocol: 'postgres',
     dialectOptions: {
@@ -26,7 +26,7 @@ module.exports = {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DATABASE_NAME,
-    host: process.env.HOSTNAME,
+    host: process.env.DB_HOST,
     dialect: 'postgres',
     protocol: 'postgres',
     dialectOptions: {
