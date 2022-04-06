2a08d10db5b8ba0c47a50c9c469a009baebba013
LeandroParisi <lepcbelisario@gmail.com>
2022-04-05 22:31:46 -0300

    adds ca certificate

--- config/config.js
+++ config/config.js
@@ -13,7 +13,7 @@ module.exports = {
     username: process.env.DATABASE_URL.split('//')[1].split(':')[0],
     password: process.env.DB_PASSWORD.split(':')[2].split('@')[0],
     database: process.env.DATABASE_NAME.split(':')[2].split('@')[0].split('/')[1],
-    host: process.env.HOSTNAME.split(':')[2].split('@')[0].split(':'[0]),
+    host: process.env.HOSTNAME.split(':')[2].split('@')[0].split(':')[0],
     dialect: 'postgres',
     protocol: 'postgres',
     // dialectOptions: {
